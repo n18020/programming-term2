59978db452330202ab5b8c950d5be877b58e4115
n18020 <n18020@std.it-college.ac.jp>
2018-07-06 11:32:01 +0900

    0705kadai

---
+++ src/basic-c2/task20180705.py
@@ -0,0 +1,21 @@
+#支払金額
+beer = 200 
+otumami = 100
+yakitori = 100
+
+#個数
+beer_c = 2
+otumami_c = 1
+yakitori_c = 2
+
+#焼き鳥の割引数
+rate = 0.2
+
+#ポイントカードから引く料金
+point = 150
+
+#会計
+payment = ((beer * beer_c) + (otumami * otumami_c) + (yakitori * (1 - rate) * yakitori_c)) - point
+
+print(payment , '円')
+
