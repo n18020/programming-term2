c6298c4e2807c62f397a5ef0e51f88b86b8c81c3
n18020 <n18020@std.it-college.ac.jp>
2018-07-10 16:45:29 +0900

    in space

--- src/basic-c2/hanaya-var.py
+++ src/basic-c2/hanaya-var.py
@@ -1,14 +1,14 @@
 #花屋の支払い金額を求める
 #値段
-rose_v =500
-sun_v =400
-turip_v =700
+rose_v = 500
+sun_v = 400
+turip_v = 700
 #個数
-rose_c =18
-sun_c =8-2
-turip_c =21-5
+rose_c = 18
+sun_c = 8-2
+turip_c = 21-5
 #割引率
-rate =0.1
+rate = 0.1
 #計算
 sum_v = (rose_v * rose_c) + (sun_v * sun_c) + (turip_v * turip_c)
 payment = sum_v * (1-rate)
