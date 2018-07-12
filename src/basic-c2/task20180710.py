295f9aab6a936ffe955411886ce5920420a87d83
n18020 <n18020@std.it-college.ac.jp>
2018-07-12 13:51:14 +0900

    format tuika

--- src/basic-c2/task20180710.py
+++ src/basic-c2/task20180710.py
@@ -3,14 +3,15 @@
 hand = input('何を出しますか？1:グー、2:チョキ、3:パー')
 
 #プレイヤーが出した手
+fmt = 'CPUが{your_hand}をだしました。あなたの負けです。'
 if hand == str(1):
-    result = 'あなたはグー、CPUがパーをだしました。あなたの負けです。'
+    s = fmt.format(your_hand = 'パー')
 elif hand == str(2):
-    result = 'あなたはチョキ、CPUがグーをだしました。あなたの負けです。'
+    s = fmt.format(your_hand = 'グー')
 elif hand == str(3):
-    result = 'あなたはパー、CPUがチョキをだしました。あなたの負けです。'
+    s = fmt.format(your_hand = 'チョキ')
 else:
-    result = '入力が不正です。終了します。'
+    s = '入力が不正です。終了します。'
 
 #出力結果
-print(result)
+print(s)
