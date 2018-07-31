005f02dbdf75774ec436f85b1e58e3c304adb545
n18020 <n18020@std.it-college.ac.jp>
2018-07-31 20:24:38 +0900

    0726kadai

---
+++ src/basic-c3/func-local.py
@@ -0,0 +1,9 @@
+#関数の外側でvalueに１００を代入
+value = 100
+
+def changeValue():
+    # 関数の内側でvalueを変更
+    value = 20
+
+changeValue()
+print('value=',value)
---
+++ src/basic-c3/task20180726_lv1.py
@@ -0,0 +1,23 @@
+#関数定義
+import random
+s = 0   #出た目
+g = 0   #進むマスの数
+# 関数
+def shake_dice():
+    s = random.randint(1, 6)
+    return s
+def go_forward(g):
+    g += s
+    return g
+
+while g < 10:
+    enter = input("サイコロを振ってください。")
+    if enter != "":
+        continue
+    s = shake_dice()
+    g = go_forward(g)
+    if g < 10:
+        print("{}が出ました。現在位置{}はです。".format(s, g))
+        continue
+    break
+print("{}が出ました。おめでとうございます、ゴールしました!".format(s))
